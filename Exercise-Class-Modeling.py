9a44bb40c8190efd028de1e6107bdcfa21046b76
OhLGitHub <OhLindseyK@gmail.com>
2022-01-03 20:28:13 -0500

    commit number 1

---
+++ Exercise-Class-Modeling.py
@@ -0,0 +1,25 @@
+#Animal
+#--------
+#+ name: string
+#+ age: int
+#+ habitat: string
+#--------
+#+ move()
+#+ vocalize()
+
+#Book
+#--------
+#+ name: string
+#+ author: string
+#+ isbn: int
+#--------
+#+ read()
+
+#Vehicle
+#--------
+#+ make: string
+#+ model: string
+#+ color: string
+#--------
+#+ move()
+#+ stop()
---
+++ README.md
@@ -0,0 +1,6 @@
+## Intro to Classes
+
+This code contains three Python classes: Animal, Book, Vehicle.
+
+This code was written as an Exercise for Object Oriented Data Modeling.
+(Dev10, Genesis10)
